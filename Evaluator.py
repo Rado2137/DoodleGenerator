6fd2d02d62fd13410d9362351919a3ba9a8b56a0
Radoslaw Lesniewski <Radoslaw.Lesniewski@sabre.com>
2017-07-03 21:56:30 +0200

    Initial commit

---
+++ Evaluator.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from Program import eval_loss_and_grads
+
+
+class Evaluator(object):
+
+    def __init__(self):
+        self.loss_value = None
+        self.grads_values = None
+
+    def loss(self, x):
+        assert self.loss_value is None
+        loss_value, grad_values = eval_loss_and_grads(x)
+        self.loss_value = loss_value
+        self.grad_values = grad_values
+        return self.loss_value
+
+    def grads(self, x):
+        assert self.loss_value is not None
+        grad_values = np.copy(self.grad_values)
+        self.loss_value = None
+        self.grad_values = None
+        return grad_values
